1f964be551dc59299d5ed86ab2e94a4b8df38853
Iuri Souza <iuri.souzasantos@gmail.com>
2024-11-04 15:39:19 -0300

    como utilizar selenium + scrapy

--- varredor_de_sites/varredor_de_sites/settings.py
+++ varredor_de_sites/varredor_de_sites/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "varredor_de_sites.spiders"
 #USER_AGENT = "varredor_de_sites (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
